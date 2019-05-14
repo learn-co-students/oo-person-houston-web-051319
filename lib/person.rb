511ea5710b9e332ce58022edb1e4ce584ec8426b
Roysan Easo <easo.roysan@gmail.com>
2019-05-14 16:38:59 -0500

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -54,53 +54,42 @@ class Person
     def clean?
         self.hygiene > 7
     end
+    
     def get_paid(salary)
-     self.bank_account=salary
-     "all about the benjamins"
+        self.bank_account=salary
+        "all about the benjamins"
     end
+
     def take_bath
-         self.hygiene=(self.hygiene + 4)
-         "♪ Rub-a-dub just relaxing in the tub ♫"
+        self.hygiene=(self.hygiene + 4)
+        "♪ Rub-a-dub just relaxing in the tub ♫"
     end
+
     def work_out 
-         self.hygiene=(self.hygiene - 3)
+        self.hygiene=(self.hygiene - 3)
         self.happiness=(self.happiness + 2)
         '♪ another one bites the dust ♫'
     end
+
     def call_friend(friend)
         self.happiness=(self.happiness + 3)
         friend.happiness=(friend.happiness + 3)
         "Hi #{friend.name}! It's #{self.name}. How are you?"
     end
+
     def start_conversation(friend,conversation)
-     case conversation
-     when "politics"
-        self.happiness=(self.happiness - 2)
-        friend.happiness=(friend.happiness - 2)
-        "blah blah partisan blah lobbyist"
-     when "weather"
-        self.happiness=(self.happiness + 1)
-        friend.happiness=(friend.happiness + 1)
-        "blah blah sun blah rain"
-     else
-        "blah blah blah blah blah"         
-     end
+        case conversation
+        when "politics"
+            self.happiness=(self.happiness - 2)
+            friend.happiness=(friend.happiness - 2)
+            "blah blah partisan blah lobbyist"
+        when "weather"
+            self.happiness=(self.happiness + 1)
+            friend.happiness=(friend.happiness + 1)
+            "blah blah sun blah rain"
+        else
+            "blah blah blah blah blah"         
+        end
     end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 end
